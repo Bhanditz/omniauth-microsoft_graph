38655a10058df5e3fa4d92be98f15ee16d06f71b
notmaxx <alexey.pikin@gmail.com>
2017-04-03 19:03:33 -0700

    rename expires_in

--- lib/omniauth/strategies/microsoft_graph.rb
+++ lib/omniauth/strategies/microsoft_graph.rb
@@ -32,7 +32,7 @@ module OmniAuth
           'org_display_name' => org_info["value"].blank? ? nil : org_info["value"][0]["displayName"],
           'org_id' => org_info["value"].blank? ? nil : org_info["value"][0]["id"],
           'chat_access_token' => options[:request_chat_access_token] ? chat_access_token['access_token'] : nil,
-          'chat_access_token_expire' => options[:request_chat_access_token] ? chat_access_token['expires_in'] : nil,
+          'chat_access_token_expires_in' => options[:request_chat_access_token] ? chat_access_token['expires_in'] : nil,
         }
       end
 
